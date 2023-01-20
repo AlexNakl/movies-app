378437bfff45f00a7620b06cc7e8d3654f4a3966
Alex Nakl <nakleskin93@yandex.ru>
2023-01-20 20:26:13 +0400

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <section className="todoapp" />;
+  }
+}
